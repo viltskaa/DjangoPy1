3ef606ca26145d228abc03463f9e8f82d9ffd620
Bazunov Andrew Igorevich <35404933+viltskaa@users.noreply.github.com>
2025-04-15 17:40:06 +0400

    Добавил фильтрацию

--- test_app/test_project/books_view.py
+++ test_app/test_project/books_view.py
@@ -6,8 +6,19 @@ from .models import Author, Book
 
 
 def get_books(request: HttpRequest) -> HttpResponse:
-    books = Book.objects.all()
+    q = request.GET.get('q')
+    pages = request.GET.get('pages')
+    author = request.GET.get('author')
+
+    result = ((Book.objects.filter(name__icontains=q)
+               | Book.objects.filter(author__name__icontains=q))
+              & Book.objects.filter(count_pages__gt=int(pages)))
+
+    if author:
+        result = result.difference(Book.objects.filter(author_id__exact=None))
 
     return render(request, 'books.html', {
-        'books': books,
+        'books': result,
+        'q': q,
+        'pages': pages,
     })
